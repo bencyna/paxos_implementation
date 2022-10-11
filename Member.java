121e5d49228c941430b07b117dbcb94be0a6fee6
Ben Cyna <benjamincyna@gmail.com>
2022-10-11 11:22:57 +1030

    init

---
+++ Member.java
@@ -0,0 +1,11 @@
+public class Member {
+    int responseDelay;
+    Boolean wantsPresidency;
+    int chancesOfResponse;
+
+    Member(int responseDelay, Boolean wantsPresidency, int chancesOfResponse) {
+        this.responseDelay = responseDelay;
+        this.wantsPresidency = wantsPresidency;
+        this.chancesOfResponse = chancesOfResponse;
+    }
+}
